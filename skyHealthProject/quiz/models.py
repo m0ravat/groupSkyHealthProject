8ccac08f9a0780fd7f42ff5a45c606d4d874834a
Muhammad Ravat <m0ravat736@gmail.com>
2025-04-12 18:48:50 +0100

    created model structure for db tables

--- skyHealthProject/quiz/models.py
+++ skyHealthProject/quiz/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+from core.models import User
 # Create your models here.
+
+
+class Session(models.Model):
+    Quarter_Choices = [("Q1", "Q1"), ("Q2", "Q2"), ("Q3", "Q3"), ("Q4", "Q4")]
+    sessionId = models.AutoField(primary_key=True)
+    sessionYear = models.PositiveSmallIntegerField(max_length=4)
+    sessionQuarter = models.CharField(choices=Quarter_Choices, max_length=2)
+
+    class Meta:
+        db_table = "Session"
+
+
+class SessionAssignment(models.Model):
+    sessionAssignId = models.AutoField(primary_key=True)
+    username = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="hasSessions")
+    sessionId = models.ForeignKey(Session, on_delete=models.SET_NULL, null=True, related_name="isAssigned")
+    additionalComments = models.TextField()
+
+    class Meta:
+        db_table = "Session_Assignment"
+
+class Card(models.Model):
+    cardId = models.AutoField(primary_key=True)
+    cardName = models.CharField(max_length=100)
+    cardDesc = models.TextField()
+
+    class Meta:
+        db_table= "Card"
+
+
+class Vote(models.Model):
+    Int_Choices = [(3, 3), (1, 1), (2, 2)]
+    voteId = models.AutoField(primary_key=True)
+    rating = models.IntegerField(choices=Int_Choices, max_length=1)
+    progress = models.IntegerField(choices=Int_Choices, max_length=1)
+    cardId = models.ForeignKey(Card, on_delete=models.SET_NULL, null=True, related_name="BelongsTo")
+    sessionAssignId = models.ForeignKey(SessionAssignment, on_delete=models.SET_NULL, null=True, related_name="hasSession")
+    class Meta:
+        db_table = "Vote"
--- skyHealthProject/user/models.py
+++ skyHealthProject/user/models.py
@@ -1,37 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    Role_Choices = [
-        ("Engineer", "Engineer"),
-        ("teamLeader", "Team Leader"),
-        ("deptLeader", "Department Leader"),
-        ("seniorManager", "Senior Manager"),
-    ]
-    username = models.CharField(max_length=50, primary_key=True)
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
-    email = models.EmailField()
-    role = models.CharField(choices=Role_Choices, max_length=50)
-    password = models.CharField(max_length=128)
-    class Meta:
-        db_table = "User"
 
-
-class Department (models.Model):
-    Dept_Choices = []
-    deptName = models.CharField(max_length=50, primary_key=True)
-    seniorManager = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="Managed_By")
-    deptLeader = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="Led_By")
-    class Meta:
-        db_table = "Department"
-
-
-class Team (models.Model):
-    Team_Choices = []
-    teamName = models.CharField(max_length=50, primary_key=True)
-    teamLeader = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="Led_Teams")
-    deptName = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, related_name="Led_Departments")
-    class Meta:
-        db_table = "Team"
 
