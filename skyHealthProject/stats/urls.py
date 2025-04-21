d8f27747cc6005162e3431673478175e526928a7
Muhammad Ravat <m0ravat736@gmail.com>
2025-04-21 16:51:58 +0100

    put css files into project wide static + basic URLs for stats app

--- skyHealthProject/stats/urls.py
+++ skyHealthProject/stats/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.stats)
+    path('', views.stats),
+    path('engineer', views.engStats),
+    path('team', views.teamStats),
+    path('department', views.deptStats)
 ]
--- skyHealthProject/stats/views.py
+++ skyHealthProject/stats/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from templates import *
 # Create your views here.
 
 def stats(request): 
-    return HttpResponse("Statistics page")
+    return render(request, 'statsPage.html')
+
+def engStats(request):
+    return render(request, 'engStats.html')
+
+def teamStats(request):
+    return render(request, 'teamStats.html')
+
+def deptStats(request):
+    return render(request, 'deptStats.html')
