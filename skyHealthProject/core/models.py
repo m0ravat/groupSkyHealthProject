4bdc710b5469fea13d76dcbdc83d305205bae1b0
iqs-dev <84716514+iqs-dev@users.noreply.github.com>
2025-04-23 16:00:31 +0100

    models.py

--- skyHealthProject/core/models.py
+++ skyHealthProject/core/models.py
@@ -34,3 +34,43 @@ class Team (models.Model):
     deptName = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, related_name="Led_Departments")
     class Meta:
         db_table = "Team"
+
+class Card(models.Model):
+    cardId = models.CharField(max_length=10, primary_key=True)
+    cardName = models.CharField(max_length=100)
+    cardDesc = models.TextField()
+
+    class Meta:
+        db_table = "Card"
+
+
+class Session(models.Model):
+    sessionId = models.CharField(max_length=10, primary_key=True)
+    sessionYear = models.CharField(max_length=10)
+    sessionQuarter = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = "Session"
+
+
+class SessionAssignment(models.Model):
+    assignId = models.CharField(max_length=10, primary_key=True)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    sessionId = models.ForeignKey(Session, on_delete=models.CASCADE)
+    additionalComments = models.TextField(null=True, blank=True)
+
+    class Meta:
+        db_table = "SessionAssignment"
+
+
+class Vote(models.Model):
+    voteId = models.CharField(max_length=10, primary_key=True)
+    rating = models.IntegerField()
+    progress = models.IntegerField()
+    cardId = models.ForeignKey(Card, on_delete=models.CASCADE)
+    sessionId = models.ForeignKey(Session, on_delete=models.CASCADE)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    assignId = models.ForeignKey(SessionAssignment, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "Vote"
