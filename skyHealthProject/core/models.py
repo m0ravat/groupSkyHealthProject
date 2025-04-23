1ad2980a8a71602785dc80b317117a03db5d314a
iqs-dev <84716514+iqs-dev@users.noreply.github.com>
2025-04-23 20:46:03 +0100

    Update models.py

--- skyHealthProject/core/models.py
+++ skyHealthProject/core/models.py
@@ -1,44 +1,21 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("Email is required")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)  # hashes password
-        user.save(using=self._db)
-        return user
-
-class User(AbstractBaseUser, PermissionsMixin):
+class User(models.Model):
     Role_Choices = [
         ("Engineer", "Engineer"),
         ("teamLeader", "Team Leader"),
         ("deptLeader", "Department Leader"),
         ("seniorManager", "Senior Manager"),
     ]
-
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=50, unique=True)
+    username = models.CharField(max_length=50, primary_key=True)
     firstName = models.CharField(max_length=50)
     lastName = models.CharField(max_length=50)
+    email = models.EmailField()
     role = models.CharField(choices=Role_Choices, max_length=50)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
+    password = models.CharField(max_length=128)
     class Meta:
         db_table = "User"
-
-    def __str__(self):
-        return self.email
         
-
 class Department (models.Model):
     Dept_Choices = []
     deptName = models.CharField(max_length=50, primary_key=True)
