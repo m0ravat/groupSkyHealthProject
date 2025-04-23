0b86965d48606a4c2988881a32a75d816e558a41
iqs-dev <84716514+iqs-dev@users.noreply.github.com>
2025-04-23 19:51:57 +0100

    Create forms.py

---
+++ skyHealthProject/core/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import User
+from captcha.fields import CaptchaField
+
+class SignupForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    captcha = CaptchaField()
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    captcha = CaptchaField()
