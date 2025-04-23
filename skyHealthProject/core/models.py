5d7712b823c6e1bd30501ebb750c89d2f11f4512
iqs-dev <84716514+iqs-dev@users.noreply.github.com>
2025-04-23 20:01:35 +0100

    Update models.py

--- skyHealthProject/core/models.py
+++ skyHealthProject/core/models.py
@@ -1,39 +1,43 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Email is required")
-        user = self.model(email=self.normalize_email(email))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)  # hashes password
         user.save(using=self._db)
         return user
 
-class User(AbstractBaseUser):
-    email = models.EmailField(unique=True)
-    USERNAME_FIELD = 'email'
-
-    objects = UserManager()
-
-    def __str__(self):
-        return self.email
-        
-class User(models.Model):
+class User(AbstractBaseUser, PermissionsMixin):
     Role_Choices = [
         ("Engineer", "Engineer"),
         ("teamLeader", "Team Leader"),
         ("deptLeader", "Department Leader"),
         ("seniorManager", "Senior Manager"),
     ]
-    username = models.CharField(max_length=50, primary_key=True)
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
     firstName = models.CharField(max_length=50)
     lastName = models.CharField(max_length=50)
-    email = models.EmailField()
     role = models.CharField(choices=Role_Choices, max_length=50)
-    password = models.CharField(max_length=128)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
     class Meta:
         db_table = "User"
 
+    def __str__(self):
+        return self.email
+        
 
 class Department (models.Model):
     Dept_Choices = []
