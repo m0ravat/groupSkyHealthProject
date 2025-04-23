23233509a43b93ea6a5178d6531277c3cfbc3621
iqs-dev <84716514+iqs-dev@users.noreply.github.com>
2025-04-23 19:52:49 +0100

    Update views.py

--- skyHealthProject/core/views.py
+++ skyHealthProject/core/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Iqra Shah (w1973224)
+def signup_view(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = SignupForm()
+    return render(request, "signup.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                request,
+                email=form.cleaned_data["email"],
+                password=form.cleaned_data["password"],
+            )
+            if user:
+                login(request, user)
+                return redirect("dashboard")
+    else:
+        form = LoginForm()
+    return render(request, "login.html", {"form": form})
+
+def dashboard(request):
+    return render(request, "dashboard.html", {"user": request.user})
