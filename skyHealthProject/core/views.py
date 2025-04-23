d0cdeaef06b846347762d9122a20513be47d358d
iqs-dev <84716514+iqs-dev@users.noreply.github.com>
2025-04-23 20:03:55 +0100

    Update views.py

--- skyHealthProject/core/views.py
+++ skyHealthProject/core/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import SignupForm, LoginForm 
+from django.contrib.auth.decorators import login_required
 
 # Iqra Shah (w1973224)
 def signup_view(request):
     if request.method == "POST":
         form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("login")
+            form.save()  
+            return redirect("login")  
     else:
         form = SignupForm()
     return render(request, "signup.html", {"form": form})
@@ -15,17 +18,14 @@ def login_view(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
-            user = authenticate(
-                request,
-                email=form.cleaned_data["email"],
-                password=form.cleaned_data["password"],
-            )
-            if user:
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
                 login(request, user)
                 return redirect("dashboard")
+            else:
+                form.add_error(None, "Invalid email or password")
     else:
         form = LoginForm()
     return render(request, "login.html", {"form": form})
-
-def dashboard(request):
-    return render(request, "dashboard.html", {"user": request.user})
