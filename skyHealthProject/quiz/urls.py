4391af1b6a47f766652aeceef11b6f986604786b
zeusxix <Bhunna1@gmail.com>
2025-04-29 17:16:10 +0100

    Update urls.py

--- skyHealthProject/quiz/urls.py
+++ skyHealthProject/quiz/urls.py
@@ -3,7 +3,7 @@ from . import views
 from core import views as core_views
 
 urlpatterns = [
-    path('', core_views.home_view, name='quiz'),
-    path('quiz/<int:session_assignment_id>/', views.quiz_view, name='quiz1'),
+    path('', core_views.home_view, name='quiz'),  # Home view from 'core' app
+    path('quiz/', views.quiz_view, name='quiz1'),  # Fixed: removed parameter
     path('complete/', views.quiz_success, name='complete'),
 ]
