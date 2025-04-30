5461e1c5835ea75ede1fb44b914de61718eefaa5
BTWITSMOHI <166065591+BTWITSMOHI@users.noreply.github.com>
2025-04-30 21:22:09 +0100

    Full Name

--- skyHealthProject/core/admin.py
+++ skyHealthProject/core/admin.py
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 # By Mohi (w1982510) and Iqra Shah (1973224)
+=======
+# By Mohiuddin Mohammed (w1982510)
+>>>>>>> Stashed changes
 
 from django.contrib import admin
 from .models import Team, Department
