d500e94e1ec79f6db52349361e6d50ce16eb18d0
zeusxix <Bhunna1@gmail.com>
2025-04-30 18:32:04 +0100

    Update forms.py

--- skyHealthProject/quiz/forms.py
+++ skyHealthProject/quiz/forms.py
@@ -1,15 +1,17 @@
 from django import forms
 from captcha.fields import CaptchaField
-from .models import Vote, Card
+from .models import Card
 
 class QuizForm(forms.Form):
-    def init(self, args, **kwargs):
+    captcha = CaptchaField()
+
+    def __init__(self, *args, **kwargs):
         cards = kwargs.pop('cards', [])
-        super(QuizForm, self).init(args, **kwargs)
+        super(QuizForm, self).__init__(*args, **kwargs)
         for card in cards:
             self.fields[f'card_{card.id}'] = forms.IntegerField(
                 label=card.question_text,
-                min_value=1, max_value=5,
+                min_value=1,
+                max_value=5,
                 widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Rate 1-5'})
             )
-    captcha = CaptchaField()
